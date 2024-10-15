1515a2365cc80b2d43b4e2317e1369d70b94f2f6
duykhai130604 <doduykhai1306.azure@gmail.com>
2024-10-15 13:46:41 +0700

    Update Lab4

--- front/3076/3076-content.php
+++ front/3076/3076-content.php
@@ -11,12 +11,10 @@ $url_path = $url_host . $matches[1][0];
 
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-<div class="type-222">
-    <div class="container">
-        <div class="row">
-            <h1 style="text-align: center;">We Fix Everything Repair Guides</h1>
-            <p style="text-align: center;">The free repair guide for everything and any devices and gadgets</p>
-        </div>
+<div class="container">
+    <div class="row">
+        <h1 class="custom-heading">We Fix Everything Repair Guides</h1>
+        <p class="custom-paragraph">The free repair guide for everything and any devices and gadgets</p>
     </div>
 </div>
 <div class="container">
@@ -24,20 +22,25 @@ $url_path = str_replace('\\', '/', $url_path);
         <div class="col-lg-4 col-md-6 col-12">
             <div class="custom-blog-card">
                 <div class="custom-img-container">
-                    <img src="./images/blog-1.jpg"
-                        alt="Blog Image">
+                    <img src="./images/blog-1.jpg" alt="Blog Image" class="custom-img">
                     <div class="custom-date-box">
                         <span class="custom-date">10</span>
                         <span class="custom-month">MAY</span>
                     </div>
                 </div>
                 <div class="custom-body">
-                    <h2 class="custom-title"><a href="">Phablets: Future of Mobile or Just a Fad?</a></h2>
+                    <h2 class="custom-title">
+                        <a href="" class="custom-title-link">Phablets: Future of Mobile or Just a Fad?</a>
+                    </h2>
                     <div class="custom-meta">
-                        <span><i class="fa fa-commenting"></i> 0 COMMENTS</span>
-                        <span><i class="fa fa-user"></i> BY ADMIN</span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-commenting custom-meta-icon"></i> 0 COMMENTS
+                        </span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-user custom-meta-icon"></i> BY ADMIN
+                        </span>
                     </div>
-                    <hr>
+                    <hr class="custom-divider">
                     <p class="custom-text">Nunc cursus leo risus non ac efficitur vel sed Mauris iaculis finibus
                         ex et viverra. Aliquam blandit ornare justo, in sagittis sem ornare sit amet. Cras
                         posuere vel ex at vulputate. Praesent volutpat consequat.</p>
@@ -48,20 +51,25 @@ $url_path = str_replace('\\', '/', $url_path);
         <div class="col-lg-4 col-md-6 col-12">
             <div class="custom-blog-card">
                 <div class="custom-img-container">
-                    <img src="./images/blog-8.jpg"
-                        alt="Blog Image">
+                    <img src="./images/blog-8.jpg" alt="Blog Image" class="custom-img">
                     <div class="custom-date-box">
                         <span class="custom-date">09</span>
                         <span class="custom-month">MAY</span>
                     </div>
                 </div>
                 <div class="custom-body">
-                    <h2 class="custom-title"><a href="">The Great Debate: iPad or Surface</a></h2>
+                    <h2 class="custom-title">
+                        <a href="" class="custom-title-link">The Great Debate: iPad or Surface</a>
+                    </h2>
                     <div class="custom-meta">
-                        <span><i class="fa fa-commenting"></i> 0 COMMENTS</span>
-                        <span><i class="fa fa-user"></i> BY ADMIN</span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-commenting custom-meta-icon"></i> 0 COMMENTS
+                        </span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-user custom-meta-icon"></i> BY ADMIN
+                        </span>
                     </div>
-                    <hr>
+                    <hr class="custom-divider">
                     <p class="custom-text">Nunc cursus leo risus non ac efficitur vel sed Mauris iaculis finibus
                         ex et viverra. Aliquam blandit ornare justo, in sagittis sem ornare sit amet. Cras
                         posuere vel ex at vulputate. Praesent volutpat consequat.</p>
@@ -72,20 +80,25 @@ $url_path = str_replace('\\', '/', $url_path);
         <div class="col-lg-4 col-md-6 col-12">
             <div class="custom-blog-card">
                 <div class="custom-img-container">
-                    <img src="./images/blog-8.jpg"
-                        alt="Blog Image">
+                    <img src="./images/blog-2.jpg" alt="Blog Image" class="custom-img">
                     <div class="custom-date-box">
                         <span class="custom-date">06</span>
                         <span class="custom-month">MAY</span>
                     </div>
                 </div>
                 <div class="custom-body">
-                    <h2 class="custom-title"><a href="">AMD demonstreert Zen processor</a></h2>
+                    <h2 class="custom-title">
+                        <a href="" class="custom-title-link">AMD demonstreert Zen processor</a>
+                    </h2>
                     <div class="custom-meta">
-                        <span><i class="fa fa-commenting"></i> 0 COMMENTS</span>
-                        <span><i class="fa fa-user"></i> BY ADMIN</span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-commenting custom-meta-icon"></i> 0 COMMENTS
+                        </span>
+                        <span class="custom-meta-item">
+                            <i class="fa fa-user custom-meta-icon"></i> BY ADMIN
+                        </span>
                     </div>
-                    <hr>
+                    <hr class="custom-divider">
                     <p class="custom-text">Nunc cursus leo risus non ac efficitur vel sed Mauris iaculis finibus
                         ex et viverra. Aliquam blandit ornare justo, in sagittis sem ornare sit amet. Cras
                         posuere vel ex at vulputate. Praesent volutpat consequat.</p>
@@ -93,10 +106,5 @@ $url_path = str_replace('\\', '/', $url_path);
                 </div>
             </div>
         </div>
-
-
-
     </div>
-</div>
-</div>
 </div>
--- front/3191/3191-content.php
+++ front/3191/3191-content.php
@@ -16,36 +16,32 @@ $url_path = str_replace('\\', '/', $url_path);
         <div class="col-md-3 custom-sidebar">
             <div class="custom-sidebar-section">
                 <h5 class="custom-sidebar-title">Categories</h5>
-                <ul class="category-list">
-                    <li>
-                        <a href="#">ACCESSORIES<span>(4)</span></a>
+                <ul class="custom-list">
+                    <li class="custom-list-item">
+                        <a href="#" class="custom-list-item-link custom-list-item-link-hover">ACCESSORIES<span class="custom-list-item-link-count">(4)</span></a>
                     </li>
-                    <li>
-                        <a href="#">BODYCARE<span>(4)</span></a>
+                    <li class="custom-list-item">
+                        <a href="#" class="custom-list-item-link custom-list-item-link-hover">BODYCARE<span class="custom-list-item-link-count">(4)</span></a>
                     </li>
-                    <li>
-                        <a href="#">HAIRCARE<span>(4)</span></a>
+                    <li class="custom-list-item">
+                        <a href="#" class="custom-list-item-link custom-list-item-link-hover">HAIRCARE<span class="custom-list-item-link-count">(4)</span></a>
                     </li>
-                    <li>
-                        <a href="#">SKINCARE<span>(4)</span></a>
+                    <li class="custom-list-item">
+                        <a href="#" class="custom-list-item-link custom-list-item-link-hover">SKINCARE<span class="custom-list-item-link-count">(4)</span></a>
                     </li>
                 </ul>
             </div>
             <div class="custom-sidebar-section">
                 <h5 class="custom-sidebar-title">Price</h5>
-                <ul class="price-list">
-                    <li><a href="#">All</a></li>
-                    <li><a href="#">$0.00 - $20.00</a></li>
-                    <li><a href="#">$20.00 - $40.00</a></li>
-                    <li><a href="#">$40.00 - $60.00</a></li>
-                    <li><a href="#">$60.00 - $80.00</a></li>
-                    <li><a href="#">$80.00 +</a></li>
+                <ul class="custom-list">
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">All</a></li>
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">$0.00 - $20.00</a></li>
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">$20.00 - $40.00</a></li>
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">$40.00 - $60.00</a></li>
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">$60.00 - $80.00</a></li>
+                    <li class="custom-list-item"><a href="#" class="custom-list-item-link custom-list-item-link-hover">$80.00 +</a></li>
                 </ul>
             </div>
-        </div>
-        <div class="col-md-9 custom-content">
-            <h2>Phần nội dung chính</h2>
-            <p>Đây là phần dành cho nội dung chính của trang web.</p>
         </div>
     </div>
 </div>
